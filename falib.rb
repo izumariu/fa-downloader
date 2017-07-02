c6b1e9cbf36c22e8d83a91546d7a2142c576f8ca
izu <izu@notamac.lan>
2017-07-02 22:59:41 +0200

    INIT

---
+++ falib.rb
@@ -0,0 +1,33 @@
+module FALib
+
+  Baseurl = "https://www.furaffinity.net"
+
+  def self.RelativePath(path)
+    [Baseurl,path].join("/").gsub(%r{(?<!https:)/+},"/")
+  end
+
+  def self.extract_sysmsg(resp)
+    resp.css('td.alt1').first.children.select{|i|i.name=="b"}.map(&:to_s).join.gsub(%r{</?[^<>]+>},"")
+  end
+
+  def self.extract_username_from_gallery(resp) # DEPRECATED
+    resp.css('div.user-name').children.select{|i|i.name=="span"}.first.children.first.to_s.match(%r{^~([A-Za-z0-9]+)'s Gallery})[1]
+  end
+
+  def self.extract_username_from_userpage(resp)
+    resp.css('td.addpad.lead').children.select{|i|i.name=="b"}[0].children[0].to_s.match(/^.(.*)/)[1]
+  end
+
+  def self.extract_relative_gallery_links(resp)
+    resp.css('figure.t-image').map{|i|i.children[1].children[0].children[0].attributes["href"].value}
+  end
+
+  def self.extract_absolute_gallery_links(resp)
+    extract_relative_gallery_links(resp).map{|i|RelativePath(i)}
+  end
+
+  def self.urlencode(url)
+    url.bytes.map(&:chr).map{|i| i.inspect.match(/\\x[A-F0-9]{2}/) ? ("%%%02x" % i.ord).upcase : i }.join
+  end
+
+end
---
+++ run.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+Dir.chdir File.dirname __FILE__
+
+require "net/http"
+require "nokogiri"
+require "io/console"
+load 'falib.rb'
+
+$USER = ARGV[0]
+ARGV.clear
+
+$USER||abort("USAGE: ruby #{__FILE__} <user>")
+
+resp = Net::HTTP.get_response URI FALib.urlencode FALib.RelativePath "/user/#{$USER}/"
+abort("Response code was HTTP #{resp.code}") if resp.code.to_i!=200
+resp = Nokogiri::HTML resp.body
+
+resp.to_s.match(%r{<b>System Message</b>})&&abort([
+  "+"*("SYSTEM MESSAGE".length+4),
+  "+ SYSTEM MESSAGE +",
+  "+"*("SYSTEM MESSAGE".length+4),
+  FALib.extract_sysmsg(resp)
+].join("\n"))
+
+$USER = FALib.extract_username_from_userpage(resp)
+$user = $USER.downcase.gsub(/[^A-Za-z0-9]/,"_")
+puts "Extracting pictures of ~#{$USER}'s gallery."
+
+Dir.mkdir("pics/#{$user}") rescue nil
+
+pagenum = 1
+
+loop do
+
+  resp = Net::HTTP.get_response URI FALib.urlencode FALib.RelativePath "/gallery/#{$USER}/#{pagenum}"
+  abort("Response code was HTTP #{resp.code}") if resp.code.to_i!=200
+  resp.body.match(%r{<i>There are no submissions to list</i>})&&break
+  puts "Page #{pagenum}"
+  resp = Nokogiri::HTML resp.body
+
+  links = FALib.extract_absolute_gallery_links(resp)
+
+  for link in links
+
+    resp2 = Net::HTTP.get_response URI FALib.urlencode link
+    resp2.body.match(%r{<img[^<>]*id="submissionImg"[^<>]*>})
+    $&||(puts("Error, skipping image");next)
+    maxres = "http:#{ $&.match(%r{data-fullview-src="([^"]+)"})[-1] }"
+    maxres.split("/")[-1].match %r{^\d+\.(.+)$}
+    $~[1].match(/^#{$user}_(.+)$/)
+    filename = $~[1]
+
+    File.exists?("pics/#{$user}/#{filename}")&&(puts("File #{maxres} exists, skipping");next)
+
+    content = Net::HTTP.get_response URI FALib.urlencode maxres
+    content.code.to_i==200||(puts("GET Content returned HTTP #{content.code}, skipping image\n");next)
+
+    File.write("pics/#{$user}/#{filename}",content.body)
+    puts "#{maxres} => pics/#{$user}/#{filename}"
+
+    puts
+    "".match(/./) # clear $&
+  end
+
+  puts "+"*IO.console.winsize[1]
+
+  pagenum += 1
+
+end
